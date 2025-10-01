6ea4d8c246426e2a49ef8f771f02eedd54e139f2
Usman <usmang@ntnu.no>
2025-10-01 19:14:11 +0200

    fix: Hide ActionBar

--- Oving6_Client/app/build.gradle.kts
+++ Oving6_Client/app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
