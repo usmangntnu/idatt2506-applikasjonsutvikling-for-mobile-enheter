77591014541d21dfbdae8dd32132d5484cc6b21a
Usman <usmang@ntnu.no>
2025-10-01 13:36:13 +0200

    feat: Create activity for the server

---
+++ Oving6_Server/app/src/main/java/com/example/oving6_server/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.oving6_server
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text)
+        Server(textView).start()
+    }
+}
