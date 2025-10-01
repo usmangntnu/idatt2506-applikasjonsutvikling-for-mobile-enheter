ad0a896760a4ebd1a3904e4c0a1d5bcbbf2ab96d
Usman <usmang@ntnu.no>
2025-10-01 14:08:18 +0200

    feat: Implement basic server functionality and UI updates

--- Oving6_Server/app/src/main/java/com/example/oving6_server/Server.kt
+++ Oving6_Server/app/src/main/java/com/example/oving6_server/Server.kt
@@ -1,4 +1,44 @@
 package com.example.oving6_server
 
-class Server {
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.net.ServerSocket
+import java.net.Socket
+
+
+
+class Server (
+    private val textView: TextView,
+    private val PORT: Int = 8080
+)  {
+    private var ui: String? = ""
+        set(str) {
+            MainScope().launch {
+                textView.text = str
+            }
+            field = str
+        }
+
+    fun start() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                ui = "Initializing Server..."
+                ServerSocket(PORT).use { serverSocket: ServerSocket ->
+                    ui = "ServerSocket established, waiting for client..."
+                    serverSocket.accept().use { clientSocket: Socket ->
+                        ui = "A Client connected to:\n$clientSocket"
+                        sendToClient(clientSocket, "Welcome Client!")
+                        readFromClient(clientSocket)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ui = e.message
+            }
+
+        }
+    }
 }
