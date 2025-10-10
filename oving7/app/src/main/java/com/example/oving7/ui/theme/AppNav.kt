919e856f499eeef1faba6975021acdee1dd0a609
Usman <usmang@ntnu.no>
2025-10-10 15:01:24 +0200

    feat: Set up navigation between main and settings screens

--- oving7/app/src/main/java/com/example/oving7/ui/theme/AppNav.kt
+++ oving7/app/src/main/java/com/example/oving7/ui/theme/AppNav.kt
@@ -1,4 +1,19 @@
-package com.example.oving7.ui.theme
+package com.example.oving7.ui
 
-class AppNav {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.*
+
+import com.example.oving7.viewmodel.FilmViewModel
+
+@Composable
+fun AppNav(viewModel: FilmViewModel) {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "main") {
+        composable("main") {
+            MainScreen(viewModel) { navController.navigate("settings") }
+        }
+        composable("settings") {
+            SettingsScreen(viewModel) { navController.popBackStack() }
+        }
+    }
 }
