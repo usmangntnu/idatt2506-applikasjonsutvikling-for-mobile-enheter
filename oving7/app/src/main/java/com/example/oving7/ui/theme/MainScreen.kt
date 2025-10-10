b68d7ec654152c3589b81b558e8a2d3d7259a284
Usman <usmang@ntnu.no>
2025-10-10 16:51:05 +0200

    feat: fix and update

--- oving7/app/src/main/java/com/example/oving7/ui/theme/MainScreen.kt
+++ oving7/app/src/main/java/com/example/oving7/ui/theme/MainScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.oving7.viewmodel.FilmViewModel
@@ -19,6 +21,8 @@ import com.example.oving7.viewmodel.FilmViewModel
 fun MainScreen(viewModel: FilmViewModel, onOpenSettings: () -> Unit) {
     val filmer by viewModel.filmer
     val bgColor by viewModel.bgColor.collectAsState()
+    val context = LocalContext.current
+
 
     val background = when (bgColor) {
         "Blue" -> Color(0xFFBBDEFB)
@@ -73,7 +77,7 @@ fun MainScreen(viewModel: FilmViewModel, onOpenSettings: () -> Unit) {
                         onClick = {
                             selectedFilter = "Alle filmer"
                             expanded = false
-                            viewModel.loadFilms(LocalContext.current)
+                            viewModel.loadFilms(context)
                         }
                     )
                     DropdownMenuItem(
@@ -110,7 +114,6 @@ fun MainScreen(viewModel: FilmViewModel, onOpenSettings: () -> Unit) {
                     },
                     trailingIcon = {
                         IconButton(onClick = {
-                            val context = LocalContext.current
                             if (selectedFilter == "Filmer etter regissør") {
                                 viewModel.filterByRegissor(filterText)
                             } else {
