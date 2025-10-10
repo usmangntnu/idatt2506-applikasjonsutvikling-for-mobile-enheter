d43dc7f214f14c5ecd4c2cbbd6c898e33a2b1522
Usman <usmang@ntnu.no>
2025-10-10 14:31:01 +0200

    feat: Add FilmDao interface

--- oving7/app/src/main/java/com/example/oving7/data/FilmDao.kt
+++ oving7/app/src/main/java/com/example/oving7/data/FilmDao.kt
@@ -1,4 +1,18 @@
 package com.example.oving7.data
 
-class FilmDao {
+import androidx.room.*
+
+@Dao
+interface FilmDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(filmer: List<Film>)
+
+    @Query("SELECT * FROM Film")
+    suspend fun getAll(): List<Film>
+
+    @Query("SELECT * FROM Film WHERE regissor = :name")
+    suspend fun getByRegissor(name: String): List<Film>
+
+    @Query("SELECT * FROM Film WHERE tittel = :title")
+    suspend fun getByTittel(title: String): List<Film>
 }
