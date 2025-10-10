463b1d1bc6651fdb74f036d5ac2fad000c52fc01
Usman <usmang@ntnu.no>
2025-10-10 14:44:32 +0200

    feat: Add PreferencesRepository using DataStore

--- oving7/app/src/main/java/com/example/oving7/data/PreferenceRepository.kt
+++ oving7/app/src/main/java/com/example/oving7/data/PreferenceRepository.kt
@@ -1,4 +1,24 @@
 package com.example.oving7.data
 
-class PreferenceRepository {
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore("settings")
+
+class PreferencesRepository(private val context: Context) {
+    companion object {
+        val BG_COLOR = stringPreferencesKey("bg_color")
+    }
+
+    val colorFlow: Flow<String> = context.dataStore.data.map {
+        it[BG_COLOR] ?: "White"
+    }
+
+    suspend fun setColor(color: String) {
+        context.dataStore.edit { it[BG_COLOR] = color }
+    }
 }
