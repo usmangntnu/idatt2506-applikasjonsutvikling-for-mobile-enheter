9ec79e3d7af3397333813232a4b3448feacaa49c
Usman <usmang@ntnu.no>
2025-10-10 14:29:40 +0200

    feat: Create Film entity

--- oving7/app/src/main/java/com/example/oving7/data/Film.kt
+++ oving7/app/src/main/java/com/example/oving7/data/Film.kt
@@ -1,4 +1,12 @@
 package com.example.oving7.data
 
-class Film {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Film(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val tittel: String,
+    val regissor: String,
+    val skuespillere: String
+)
