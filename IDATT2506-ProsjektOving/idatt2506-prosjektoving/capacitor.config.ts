a1117ab4a4364b633e89275c29a53da1d30c0c39
Usman <usmang@ntnu.no>
2025-10-24 13:28:19 +0200

    chore: initialize Ionic React app and Capacitor integration

---
+++ IDATT2506-ProsjektOving/idatt2506-prosjektoving/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'idatt2506-prosjektoving',
+  webDir: 'dist'
+};
+
+export default config;
