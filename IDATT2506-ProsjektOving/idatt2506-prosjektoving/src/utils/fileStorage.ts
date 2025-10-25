5a31771bfbc0dd7b8e7869f95776ba047818961e
Usman <usmang@ntnu.no>
2025-10-25 13:55:08 +0200

    feat: add method saveLists

--- IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/utils/fileStorage.ts
+++ IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/utils/fileStorage.ts
@@ -1,5 +1,16 @@
 import {Filesystem, Directory, Encoding} from '@capacitor/filesystem';
 import {ToDoList} from "../models/types";
 
+//Name of file for saving and retrieving data
 const FILE_NAME = 'lists.json';
+
+//Saves the provided lists to a file in JSON format
+export const saveLists = async (lists: TodoList[]) => {
+    await Filesystem.writeFile({
+        path: FILE_NAME,
+        data: JSON.stringify(lists),
+        directory: Directory.Data,
+        encoding: Encoding.UTF8
+    });
+};
 
