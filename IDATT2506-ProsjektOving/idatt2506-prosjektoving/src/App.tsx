0840da08612d3dec4de0a52608e4af3013242874
Usman <usmang@ntnu.no>
2025-10-26 14:17:03 +0100

    feat: Create state-variables. Lists contains all the todolists and the index points to which list is currently being viewed

--- IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/App.tsx
+++ IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/App.tsx
@@ -5,3 +5,8 @@ import ListTabs from './components/ListTabs';
 import TodoInput from './components/TodoInput';
 import TodoListView from './components/TodoListView';
 import { readLists, saveLists } from './utils/fileStorage';
+
+const App: React.FC = () => {
+    const [lists, setLists] = useState<TodoList[]>([]);
+    const [activeListId, setActiveListId] = useState<number>(0);
+}
