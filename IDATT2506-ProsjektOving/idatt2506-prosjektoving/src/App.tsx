efbc761a14ecefd33549e26294d5e410087666c1
Usman <usmang@ntnu.no>
2025-10-26 14:43:49 +0100

    feat: Some minor fix and add userinterface

--- IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/App.tsx
+++ IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/App.tsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { IonApp} from "@ionic/react";
-import { TodoList } from "./models/types"
+import { ToDoList } from "./models/types"
 import ListTabs from './components/ListTabs';
 import TodoInput from './components/TodoInput';
 import TodoListView from './components/TodoListView';
 import { readLists, saveLists } from './utils/fileStorage';
 
 const App: React.FC = () => {
-    const [lists, setLists] = useState<TodoList[]>([]);
+    const [lists, setLists] = useState<ToDoList[]>([]);
     const [activeListId, setActiveListId] = useState<number>(0);
-}
 
     // Load saved lists
     useEffect(() => {
@@ -41,3 +40,33 @@ const App: React.FC = () => {
         newLists[activeListIndex].items.unshift ({ id: Date.now().toString(), text, done: false });
         setLists(newLists);
     };
+
+    // Toggle item done status
+    const toggleDone = (id: string) => {
+        const newLists = [...lists];
+        const item = newLists[activeListIndex].items.find(i => i.id === id);
+        if (item) {
+            item.done = !item.done;
+            // Move finished items to the bottom
+            newLists[activeListIndex].items.sort((a, b) => Number(a.done) - Number(b.done));
+            setLists(newLists);
+        }
+    };
+
+    return (
+        <IonApp>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Handle-/Todoliste</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <ListTabs lists={lists} activeIndex={activeListIndex} setActiveIndex={setActiveListIndex} addList={addList} deleteList={deleteList}/>
+                <TodoInput addTodo={addTodo}/>
+                {lists[activeListIndex] && <TodoListView items={lists[activeListIndex].items} toggleDone={toggleDone}/>}
+            </IonContent>
+        </IonApp>
+    );
+};
+
+export default App;
