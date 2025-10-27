bd60c861ababde9efdcade653da6c865cff347ad
Usman <usmang@ntnu.no>
2025-10-27 15:30:08 +0100

    chore: use App.css

--- IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/components/ListTabs.tsx
+++ IDATT2506-ProsjektOving/idatt2506-prosjektoving/src/components/ListTabs.tsx
@@ -21,7 +21,7 @@ const ListTabs: React.FC<Props> = ({ lists, activeIndex, setActiveIndex, addList
             {/* KRAV 3: IonSegment used for tab-based list overview */}
             <IonSegment value={activeIndex.toString()}>
                 {lists.map((list, idx) => (
-                    <div key={idx} style={{ display: 'flex', alignItems: 'center' }}>
+                    <div key={idx} className="list-tab-container">
                         <IonSegmentButton value={idx.toString()} onClick={() => setActiveIndex(idx)}>
                             <IonLabel>{list.name}</IonLabel>
                         </IonSegmentButton>
@@ -29,8 +29,8 @@ const ListTabs: React.FC<Props> = ({ lists, activeIndex, setActiveIndex, addList
                         <IonButton
                             color="danger"
                             size="small"
+                            className="delete-button"
                             onClick={() => deleteList(idx)}
-                            style={{ marginLeft: '4px' }}
                         >
                             X
                         </IonButton>
@@ -39,7 +39,12 @@ const ListTabs: React.FC<Props> = ({ lists, activeIndex, setActiveIndex, addList
             </IonSegment>
 
             {/* KRAV 1: Input field and button for creating new lists */}
-            <input value={newListName} onChange={e => setNewListName(e.target.value)} placeholder="Ny liste"/>
+            <input
+                className="new-list-input"
+                value={newListName}
+                onChange={e => setNewListName(e.target.value)}
+                placeholder="Ny liste"
+            />
             <IonButton onClick={() => { if(newListName) { addList(newListName); setNewListName(''); }}}>Legg til liste</IonButton>
         </>
     );
